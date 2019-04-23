4a3aec779f411f155f339948b1d3c02a3c9b696c
Andrew Rice <andrewrice@Andrews-MacBook.hsd1.wa.comcast.net>
2019-04-23 11:48:34 -0700

    Updates

---
+++ precache-manifest.caf739d301bfb30624dd7c5cee33890a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cf4d94a39890a53243388df439665cb",
+    "url": "/simpson-react-clicky/index.html"
+  },
+  {
+    "revision": "c9b1eea71c3203de1f10",
+    "url": "/simpson-react-clicky/static/css/main.6d82b7c6.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/simpson-react-clicky/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "c9b1eea71c3203de1f10",
+    "url": "/simpson-react-clicky/static/js/main.b717e104.chunk.js"
+  },
+  {
+    "revision": "9301aac537982fe63ccf",
+    "url": "/simpson-react-clicky/static/js/runtime~main.7e096e58.js"
+  }
+]);
