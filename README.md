c3483d79597c56a6ff0b78a478726ad6e038ca57
Andrew Rice <andrewrice@Andrews-MacBook.hsd1.wa.comcast.net>
2019-04-23 11:43:33 -0700

    Final app push

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# simpson-react-clicky
+# Simpsons Clicky Game
+
+#### Test your memory with your favorite friends from *The Simpsons*.
+
+#### Click on each character once to win the game. But don't click on a character twice, or it's game over for you!
+
+#### Built with React. Check it out [here](https://ajrice1018.github.io/react-clicky-game/).
+
+![screenshot](public/screenshot.png)
---
+++ build/precache-manifest.176298f68ffd0f2b6e83390d5ba0a0f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac5428a7d3ef7ee688d9aba536ccfca3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9b1eea71c3203de1f10",
+    "url": "/static/css/main.6d82b7c6.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "c9b1eea71c3203de1f10",
+    "url": "/static/js/main.b717e104.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
